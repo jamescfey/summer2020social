125387be73edb894eb5938e0267fe682883d3ca5
jamescfey <jamescfey@gmail.com>
2020-07-31 12:18:07 -0700

    Added customization function and social scarf behavior (timer is untested)

--- main.py
+++ main.py
@@ -1,31 +1,22 @@
 def displayState():
     if state == 0:
-        basic.show_leds("""
-            . # # # .
-            . # . . .
-            . # # . .
-            . # . . .
-            . # . . .
-            """)
-        strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
+        Free_Icon.show_image(0)
+        Public_LEDs.show_color(Free_Color)
+        if Public_LEDs_On:
+            Private_LEDs.show_color(Free_Color)
+            Private_LEDs2.show_color(Free_Color)
     elif state == 1:
-        basic.show_leds("""
-            . # # . .
-            . # . # .
-            . # # . .
-            . # . # .
-            . # # . .
-            """)
-        strip.show_color(neopixel.colors(NeoPixelColors.RED))
+        Busy_Icon.show_image(0)
+        Public_LEDs.show_color(Busy_Color)
+        if Public_LEDs_On:
+            Private_LEDs.show_color(Busy_Color)
+            Private_LEDs2.show_color(Busy_Color)
     else:
-        basic.show_leds("""
-            . . # . .
-            . # . # .
-            # # # # #
-            # . . . #
-            # . . . #
-            """)
-        strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+        Ask_Me_Icon.show_image(0)
+        Public_LEDs.show_color(Ask_Me_Color)
+        if Public_LEDs_On:
+            Private_LEDs.show_color(Ask_Me_Color)
+            Private_LEDs2.show_color(Ask_Me_Color)
 # Case 0 is Ping, Case 1 is Set State
 # Switches Case or Sends Free if in case 2
 
@@ -39,9 +30,43 @@ def on_button_pressed_a():
         basic.show_icon(IconNames.FABULOUS)
     elif case_ == 2:
         radio.send_string("free")
+        displayState()
         case_ = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def Customize():
+    global Free_Icon, Free_Color, Busy_Icon, Busy_Color, Ask_Me_Icon, Ask_Me_Color, Public_LEDs_On, Social_Scarf, Timer_length
+    Free_Icon = images.create_image("""
+        . # # # .
+        . # . . .
+        . # # . .
+        . # . . .
+        . # . . .
+        """)
+    Free_Color = neopixel.colors(NeoPixelColors.GREEN)
+    Busy_Icon = images.create_image("""
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        """)
+    Busy_Color = neopixel.colors(NeoPixelColors.RED)
+    Ask_Me_Icon = images.create_image("""
+        . . # . .
+        . # . # .
+        # # # # #
+        # . . . #
+        # . . . #
+        """)
+    Ask_Me_Color = neopixel.colors(NeoPixelColors.YELLOW)
+    # Turns on the ends of the LED strands.
+    Public_LEDs_On = True
+    # Social Scarf Will automattically check if your friend is free based on your timer length and notify you
+    Social_Scarf = True
+    # (In Minutes)
+    # Timer length is used to handle social scarf's auto ping or sporty scarfs reminder.
+    Timer_length = 45
 # case 0 is base, case 1 is change personal state, case 2 is communicate
 
 def on_received_string(receivedString):
@@ -52,13 +77,32 @@ def on_received_string(receivedString):
         elif state == 1:
             radio.send_string("busy")
         else:
+            strip.show_rainbow(1, 360)
+            basic.pause(2000)
             basic.show_string(receivedString)
             case_ = 2
-            strip.show_bar_graph(radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH),
-                255)
     elif receivedString == "free" or receivedString == "busy":
         basic.show_string(receivedString)
         case_ = 0
+    elif receivedString == "\"AutoPing\"":
+        if state == 0:
+            radio.send_string("Autofree")
+        elif state == 1:
+            radio.send_string("Autobusy")
+        else:
+            radio.send_string("Autoask")
+    elif receivedString == "\"Autofree\"":
+        strip.show_rainbow(1, 360)
+        control.wait_micros(2000000)
+        basic.show_string("\"Your Friend is Free!\"")
+        displayState()
+    elif receivedString == "\"Autobusy\"":
+        pass
+    elif receivedString == "\"Autoask\"":
+        strip.show_rainbow(1, 360)
+        control.wait_micros(2000000)
+        basic.show_string("\"Maybe check on your friend?\"")
+        displayState()
 radio.on_received_string(on_received_string)
 
 # Displays waiting icon until it receives a reply
@@ -73,17 +117,60 @@ def on_button_pressed_b():
         displayState()
     elif case_ == 2:
         radio.send_string("busy")
+        displayState()
         case_ = 0
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+"""
+
+On Button A changes the mode as well as answers Yes when asked if free.
+
+On Button B changes your interruption state if in mode 0 or pings the other person if in mode 1 as well as answers No when asked if free.
+
+On radio received handles all potential messages 
+
+DisplayState is called to show the leds depending on the interruption status
+
+On shake clears the current LED status
+
+On start sets up the LED zones as well as handles some timer stuff.
+
+"""
 
 def on_gesture_shake():
     strip.clear()
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
 
+Social_Scarf = False
+Ask_Me_Color = 0
+Ask_Me_Icon: Image = None
+Busy_Color = 0
+Busy_Icon: Image = None
+Public_LEDs_On = False
+Free_Color = 0
+Free_Icon: Image = None
+Timer_length = 0
+Private_LEDs2: neopixel.Strip = None
+Private_LEDs: neopixel.Strip = None
+Public_LEDs: neopixel.Strip = None
 strip: neopixel.Strip = None
 state = 0
 case_ = 0
+Customize()
 done = 0
 case_ = 0
 state = 0
 strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
+Public_LEDs = strip.range(8, 12)
+Private_LEDs = strip.range(0, 7)
+Private_LEDs2 = strip.range(13, 20)
+social_count = Timer_length * 60
+# Runs social scarf code in the background and sets the timer again (untested)
+
+def on_in_background():
+    global social_count
+    if Social_Scarf:
+        if timeanddate.seconds_since_reset() == social_count:
+            radio.send_string("AutoPing")
+            social_count = social_count / timeanddate.seconds_since_reset()
+control.in_background(on_in_background)
--- main.ts
+++ main.ts
@@ -1,39 +1,30 @@
-function displayState() {
+function displayState () {
     if (state == 0) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # . .
-            . # . . .
-            . # . . .
-            `)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        Free_Icon.showImage(0)
+        Public_LEDs.showColor(Free_Color)
+        if (Public_LEDs_On) {
+            Private_LEDs.showColor(Free_Color)
+            Private_LEDs2.showColor(Free_Color)
+        }
     } else if (state == 1) {
-        basic.showLeds(`
-            . # # . .
-            . # . # .
-            . # # . .
-            . # . # .
-            . # # . .
-            `)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+        Busy_Icon.showImage(0)
+        Public_LEDs.showColor(Busy_Color)
+        if (Public_LEDs_On) {
+            Private_LEDs.showColor(Busy_Color)
+            Private_LEDs2.showColor(Busy_Color)
+        }
     } else {
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # # # # #
-            # . . . #
-            # . . . #
-            `)
-        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+        Ask_Me_Icon.showImage(0)
+        Public_LEDs.showColor(Ask_Me_Color)
+        if (Public_LEDs_On) {
+            Private_LEDs.showColor(Ask_Me_Color)
+            Private_LEDs2.showColor(Ask_Me_Color)
+        }
     }
-    
 }
-
-//  Case 0 is Ping, Case 1 is Set State
-//  Switches Case or Sends Free if in case 2
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+// Case 0 is Ping, Case 1 is Set State
+// Switches Case or Sends Free if in case 2
+input.onButtonPressed(Button.A, function () {
     if (case_ == 0) {
         case_ = 1
         displayState()
@@ -42,33 +33,83 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
         basic.showIcon(IconNames.Fabulous)
     } else if (case_ == 2) {
         radio.sendString("free")
+        displayState()
         case_ = 0
     }
-    
 })
-//  case 0 is base, case 1 is change personal state, case 2 is communicate
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    
+function Customize () {
+    Free_Icon = images.createImage(`
+        . # # # .
+        . # . . .
+        . # # . .
+        . # . . .
+        . # . . .
+        `)
+    Free_Color = neopixel.colors(NeoPixelColors.Green)
+    Busy_Icon = images.createImage(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+    Busy_Color = neopixel.colors(NeoPixelColors.Red)
+    Ask_Me_Icon = images.createImage(`
+        . . # . .
+        . # . # .
+        # # # # #
+        # . . . #
+        # . . . #
+        `)
+    Ask_Me_Color = neopixel.colors(NeoPixelColors.Yellow)
+    // Turns on the ends of the LED strands.
+    Public_LEDs_On = true
+    // Social Scarf Will automattically check if your friend is free based on your timer length and notify you
+    Social_Scarf = true
+    // (In Minutes)
+    // Timer length is used to handle social scarf's auto ping or sporty scarfs reminder.
+    Timer_length = 45
+}
+// case 0 is base, case 1 is change personal state, case 2 is communicate
+radio.onReceivedString(function (receivedString) {
     if (receivedString == "You Free?") {
         if (state == 0) {
             radio.sendString("free")
         } else if (state == 1) {
             radio.sendString("busy")
         } else {
+            strip.showRainbow(1, 360)
+            basic.pause(2000)
             basic.showString(receivedString)
             case_ = 2
-            strip.showBarGraph(radio.receivedPacket(RadioPacketProperty.SignalStrength), 255)
         }
-        
     } else if (receivedString == "free" || receivedString == "busy") {
         basic.showString(receivedString)
         case_ = 0
+    } else if (receivedString == "\"AutoPing\"") {
+        if (state == 0) {
+            radio.sendString("Autofree")
+        } else if (state == 1) {
+            radio.sendString("Autobusy")
+        } else {
+            radio.sendString("Autoask")
+        }
+    } else if (receivedString == "\"Autofree\"") {
+        strip.showRainbow(1, 360)
+        control.waitMicros(2000000)
+        basic.showString("\"Your Friend is Free!\"")
+        displayState()
+    } else if (receivedString == "\"Autobusy\"") {
+    	
+    } else if (receivedString == "\"Autoask\"") {
+        strip.showRainbow(1, 360)
+        control.waitMicros(2000000)
+        basic.showString("\"Maybe check on your friend?\"")
+        displayState()
     }
-    
 })
-//  Displays waiting icon until it receives a reply
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+// Displays waiting icon until it receives a reply
+input.onButtonPressed(Button.B, function () {
     if (case_ == 0) {
         basic.showIcon(IconNames.Surprised)
         radio.sendString("You Free?")
@@ -77,17 +118,56 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
         displayState()
     } else if (case_ == 2) {
         radio.sendString("busy")
+        displayState()
         case_ = 0
     }
-    
 })
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+/**
+ * On Button A changes the mode as well as answers Yes when asked if free.
+ * 
+ * On Button B changes your interruption state if in mode 0 or pings the other person if in mode 1 as well as answers No when asked if free.
+ * 
+ * On radio received handles all potential messages 
+ * 
+ * DisplayState is called to show the leds depending on the interruption status
+ * 
+ * On shake clears the current LED status
+ * 
+ * On start sets up the LED zones as well as handles some timer stuff.
+ */
+input.onGesture(Gesture.Shake, function () {
     strip.clear()
 })
-let strip : neopixel.Strip = null
+let Social_Scarf = false
+let Ask_Me_Color = 0
+let Ask_Me_Icon: Image = null
+let Busy_Color = 0
+let Busy_Icon: Image = null
+let Public_LEDs_On = false
+let Free_Color = 0
+let Free_Icon: Image = null
+let Timer_length = 0
+let Private_LEDs2: neopixel.Strip = null
+let Private_LEDs: neopixel.Strip = null
+let Public_LEDs: neopixel.Strip = null
+let strip: neopixel.Strip = null
 let state = 0
 let case_ = 0
+Customize()
 let done = 0
 case_ = 0
 state = 0
 strip = neopixel.create(DigitalPin.P2, 20, NeoPixelMode.RGB)
+Public_LEDs = strip.range(8, 12)
+Private_LEDs = strip.range(0, 7)
+Private_LEDs2 = strip.range(13, 20)
+let social_count = Timer_length * 60
+// Runs social scarf code in the background and sets the timer again (untested)
+control.inBackground(function () {
+    if (Social_Scarf) {
+        if (timeanddate.secondsSinceReset() == social_count) {
+            radio.sendString("AutoPing")
+            social_count = social_count / timeanddate.secondsSinceReset()
+        }
+    }
+})
